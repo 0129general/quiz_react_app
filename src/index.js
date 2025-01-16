a1c75785bc7470ab49f93f119bac1538484a760e
bojanpavlovic <bojanpavlovicfl@gmail.com>
2025-01-16 01:25:50 -0600

    update the index file

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import AppProvider from "./context";
+import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
+const root = createRoot(document.getElementById("root"));
+root.render(
 	<React.StrictMode>
 		<AppProvider>
 			<App />
 		</AppProvider>
-	</React.StrictMode>,
-	document.getElementById("root")
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
